733da8451445e2dc03ec80b085f7b71919cb95f9
Raouf Aghrout <aghrout.raouf@gmail.com>
2015-08-04 22:33:25 +0100

    removed unused mother code

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Testing - Builder, Mother and Factory
+# Builder, Mother and Factory
 
 Builder, Mother and Factory testing patterns with a simple test driven example.
 
--- src/test/java/io/ra/testingjava/mother/Mother.java
+++ src/test/java/io/ra/testingjava/mother/Mother.java
@@ -4,8 +4,6 @@ import java.lang.reflect.Field;
 import java.util.HashMap;
 import java.util.Map;
 
-import static org.apache.commons.lang3.reflect.FieldUtils.*;
-
 public class Mother<T> {
 
     private Map<String, Object> objectMap = new HashMap<>();
@@ -18,16 +16,6 @@ public class Mother<T> {
     public Mother<T> with(String propertyName, Object value) {
         objectMap.put(propertyName, value);
         return this;
-    }
-
-    public static <E> E updateField(E targetObject, String fieldName, Object value) {
-        try {
-            getDeclaredField(targetObject.getClass(), fieldName, true).set(targetObject, value);
-        } catch (IllegalAccessException e) {
-            throw new RuntimeException("Cannot find the field: " + fieldName + "!");
-        }
-
-        return targetObject;
     }
 
     public T build() {
